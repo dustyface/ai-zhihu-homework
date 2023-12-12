6e34c80514eba6c1da72d0e6e2135abe052cad6f
dustyface <dustyface@163.com>
2023-12-12 16:28:26 +0800

    try fix handle_parsing_error
    Signed-off-by: dustyface <dustyface@163.com>

--- LangChain/langchain_agent2.py
+++ LangChain/langchain_agent2.py
@@ -1,5 +1,7 @@
 from langchain.utilities import SerpAPIWrapper
-from langchain.llms import OpenAI
+
+# from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 
@@ -7,7 +9,8 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-llm = OpenAI(temperature=0)
+llm = ChatOpenAI(model="gpt-4", temperature=0)
+# llm = OpenAI(temperature=0)  # 续写model，本例无法适用
 search = SerpAPIWrapper()
 
 tools = [
@@ -18,11 +21,23 @@ tools = [
     )
 ]
 
+
+# bugfix: 暂时用这个办法，让第一步predict的结果可以被跳过，连接到下一步执行;
+# 但未解决根本问题
+# def handle_parsing_errors(error) -> str:
+#     aftercolon = str(error).split(":")[-1].strip()
+#     if aftercolon.startswith("Yes"):
+#         # return AgentAction("Intermediate Answer", aftercolon, error)
+#         return aftercolon
+#     return str(error)
+
+
 self_ask_with_search = initialize_agent(
     tools,
     llm,
     agent=AgentType.SELF_ASK_WITH_SEARCH,
     verbose=True,
+    # handle_parsing_errors=handle_parsing_errors,
     handle_parsing_errors=True,
 )
 self_ask_with_search.run("吴京的老婆的主持过哪些节目")
